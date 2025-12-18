c24d7234a71499242e3ff9791d7cb41129ed3318
Nguyen-Thanh-Tri <2212thanhttricat@gmail.com>
2025-12-18 17:38:40 +0700

    FEAT:
    +Add icon application
    
    +Flash screen

---
+++ app/src/main/java/com/mtp/shedule/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.mtp.shedule;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen") // Bỏ qua cảnh báo Android 12
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Chờ 2 giây (2000ms) rồi chuyển sang màn hình chính
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Đóng Splash để user không back lại được
+        }, 2000);
+    }
+}
