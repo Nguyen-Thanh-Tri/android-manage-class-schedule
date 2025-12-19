1758767cae0eb3993374773d5c1494b07b832937
Nguyen-Thanh-Tri <2212thanhttricat@gmail.com>
2025-12-19 18:56:44 +0700

    min26, target34, compile36
    androidx.activity:activity:1.11.0 require sdk 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 
 android {
     namespace = "com.mtp.shedule"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.mtp.shedule"
         minSdk = 26
         //noinspection OldTargetApi
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
